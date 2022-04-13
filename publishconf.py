2188d97bbc55849d933fdabc10dd356e11d83d25
Andrey Antukh <niwi@niwi.nz>
2022-04-13 10:51:21 +0200

    Minor changes on publish conf

--- publishconf.py
+++ publishconf.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://www.niwi.nz'
+SITEURL = 'https://niwi.nz'
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
